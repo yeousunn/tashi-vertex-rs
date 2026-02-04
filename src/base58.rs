6a0823c4f334e89e9c4d93c2ffd419d1e7e4e1ad
Asher <179183456+asher84@users.noreply.github.com>
2026-02-03 19:06:26 -0800

    fix(base58): adjust for in-out output len examples
    Signed-off-by: Asher <179183456+asher84@users.noreply.github.com>

--- src/base58.rs
+++ src/base58.rs
@@ -1,4 +1,3 @@
-use std::ffi::{CStr, CString};
 use std::str::from_utf8_unchecked;
 
 use crate::error::TVResult;
@@ -11,18 +10,21 @@ pub const fn encode_length(input_len: usize) -> usize {
 }
 
 /// Encodes a byte array into a Base58 string.
-pub fn encode(input: &[u8], output: &mut [u8]) -> crate::Result<()> {
+pub fn encode(input: &[u8], output: &mut [u8]) -> crate::Result<usize> {
+    let mut output_len = output.len();
+
     let res = unsafe {
-        // FIXME: output length should be an in/out parameter
         tv_base58_encode(
             input.as_ptr(),
             input.len(),
             output.as_mut_ptr(),
-            output.len(),
+            &mut output_len,
         )
     };
 
-    res.ok()
+    res.ok()?;
+
+    Ok(output_len)
 }
 
 /// Encodes a byte array into a Base58 string.
@@ -32,11 +34,11 @@ pub(crate) fn with_encoded<const N: usize, R>(
 ) -> crate::Result<R> {
     let mut output = [0_u8; N];
 
-    // FIXME: update usage once this returns actual length
-    encode(input, &mut output)?;
+    let output_len = encode(input, &mut output)?;
+    let output = &output[..output_len];
 
-    let s = unsafe { CStr::from_bytes_with_nul_unchecked(&output) };
-    let s = unsafe { from_utf8_unchecked(s.to_bytes()) };
+    // SAFE: output is guaranteed to be UTF-8
+    let s = unsafe { from_utf8_unchecked(output) };
 
     Ok(callback(s))
 }
@@ -45,15 +47,12 @@ pub(crate) fn with_encoded<const N: usize, R>(
 pub fn encode_to_string(input: &[u8]) -> crate::Result<String> {
     let mut output = vec![0u8; encode_length(input.len())];
 
-    // FIXME: update usage once this returns actual length
-    encode(input, &mut output)?;
+    let output_len = encode(input, &mut output)?;
 
-    // convert through CString to remove null termination
-    // SAFE: output is guaranteed to be null-terminated and UTF-8
-    let s = unsafe { CString::from_vec_with_nul_unchecked(output) };
-    let s = unsafe { String::from_utf8_unchecked(s.into_bytes()) };
+    output.truncate(output_len);
 
-    Ok(s)
+    // SAFE: output is guaranteed to be UTF-8
+    Ok(unsafe { String::from_utf8_unchecked(output) })
 }
 
 /// Calculates the maximum length of a decoded byte array
@@ -64,18 +63,21 @@ pub const fn decode_length(input_len: usize) -> usize {
 }
 
 /// Decodes a Base58 string into a byte array.
-pub fn decode(input: &[u8], output: &mut [u8]) -> crate::Result<()> {
+pub fn decode(input: &[u8], output: &mut [u8]) -> crate::Result<usize> {
+    let mut output_len = output.len();
+
     let res = unsafe {
-        // FIXME: output length should be an in/out parameter
         tv_base58_decode(
             input.as_ptr(),
             input.len(),
             output.as_mut_ptr(),
-            output.len(),
+            &mut output_len,
         )
     };
 
-    res.ok()
+    res.ok()?;
+
+    Ok(output_len)
 }
 
 /// Decodes a Base58 string into a byte array.
@@ -85,18 +87,18 @@ pub(crate) fn with_decoded<const N: usize, R>(
 ) -> crate::Result<R> {
     let mut output = [0_u8; N];
 
-    // FIXME: update usage once this returns actual length
-    decode(input, &mut output)?;
+    let output_len = decode(input, &mut output)?;
 
-    Ok(callback(&output[..]))
+    Ok(callback(&output[..output_len]))
 }
 
 /// Decodes a Base58 string into a byte array.
 pub fn decode_to_vec(input: &[u8]) -> crate::Result<Vec<u8>> {
     let mut output = vec![0u8; decode_length(input.len())];
 
-    // FIXME: update usage once this returns actual length
-    decode(input, &mut output)?;
+    let output_len = decode(input, &mut output)?;
+
+    output.truncate(output_len);
 
     Ok(output)
 }
@@ -106,13 +108,13 @@ unsafe extern "C" {
         input: *const u8,
         input_len: usize,
         output: *mut u8,
-        output_len: usize,
+        output_len: *mut usize,
     ) -> TVResult;
 
     fn tv_base58_decode(
         input: *const u8,
         input_len: usize,
         output: *mut u8,
-        output_len: usize,
+        output_len: *mut usize,
     ) -> TVResult;
 }
